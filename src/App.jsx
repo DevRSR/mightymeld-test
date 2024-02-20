89cc29683762f9d781ec255161b0feb9f75d53ea
DevRSR <rajiridwanullah12@@gmail.com>
2024-02-20 15:43:09 +0100

    .....

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 
 function App() {
   return 
-  <form />
-  <div className={" w-full h-screen"}></div>;
+  (
+  <div className={" w-full h-screen"}></div>
+  );
 }
 
 export default App;
