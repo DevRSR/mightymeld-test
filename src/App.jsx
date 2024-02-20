60fac64309072bc871445ebcd3a48f5fd07f204d
DevRSR <rajiridwanullah12@@gmail.com>
2024-02-20 15:31:20 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+ 
+
+  return (
+      <div className=' w-24 h-24 bg-yellow-500'>
+        
+      </div>
+      
+  )
+}
+
+export default App
