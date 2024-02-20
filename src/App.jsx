9ac655c4eb1a52ed422194e4cf7c98f3d4c13e76
DevRSR <rajiridwanullah12@@gmail.com>
2024-02-20 16:11:48 +0100

    app settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 function App() {
-  return;
-  <div className=" w-full h-full">
-    <form className="w-2/3 h-2/3 m-auto">
-      <div>Login Page</div>
-    </form>
-  </div>;
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
