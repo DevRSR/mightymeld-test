529298ae1c3d14ad71c9ebf09298438a78a918c4
DevRSR <rajiridwanullah12@@gmail.com>
2024-02-20 15:50:20 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 
 function App() {
-  return 
-  (
-    <div>
-      <div className={" w-full h-screen"}>
-        
-      </div>
-    </div>
-  
-  );
+  return;
+  <div className=" w-full h-full">
+    <form className="w-2/3 h-2/3 m-auto">
+      <div>Login Page</div>
+    </form>
+  </div>;
 }
 
 export default App;
